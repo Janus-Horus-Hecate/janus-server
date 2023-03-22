6484d94636d6fac1ede3a192891e2737ae0a3a46
lancenonce <lancendavis47@gmail.com>
2023-03-22 13:36:43 -0400

    Baseline Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use eyre::Result;
+use log::{error, info};
+use rpc_server::run_server;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    colog::init();
+    let (addr, server_handle) = run_server().await.unwrap();
+    let url = format!("http://{}", addr);
+    println!("Server started, listening on {url}");
+
+    server_handle.stopped().await;
+
+    Ok(())
 }
